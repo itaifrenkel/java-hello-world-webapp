0825182a4d601f67db0e682df90e094450d38c7e
John Devenish <johnd4@discovery.co.za>
2020-01-01 10:58:21 +0200

    Basic action invocation

--- src/main/java/com/github/dagwud/woodlands/game/commands/natives/SetVarAction.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/natives/SetVarAction.java
@@ -3,12 +3,12 @@ package com.github.dagwud.woodlands.game.commands.natives;
 import com.github.dagwud.woodlands.game.commands.invocation.ActionResults;
 
 @SuppressWarnings("unused") // called at runtime via reflection
-public class ReadTextInputAction extends NativeAction
+public class SetVarAction extends NativeAction
 {
   @Override
   public ActionResults invoke()
   {
-    System.out.println("READ TEXT");
+    System.out.println("SET VAR");
     return new ActionResults();
   }
 }
