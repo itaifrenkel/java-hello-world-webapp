32911a30c457a2aed116a96488f12a92f9d5f98c
John Devenish <johnd4@discovery.co.za>
2020-01-04 12:22:01 +0200

    Cleanup

--- src/main/java/com/github/dagwud/woodlands/game/GameState.java
+++ src/main/java/com/github/dagwud/woodlands/game/GameState.java
@@ -1,16 +1,11 @@
 package com.github.dagwud.woodlands.game;
 
-import com.github.dagwud.woodlands.game.commands.invocation.ActionInvokerDelegate;
-import com.github.dagwud.woodlands.game.commands.invocation.VariableStack;
-
 public class GameState
 {
   private static GameState instance;
-  private ActionInvokerDelegate actionInvokerDelegate;
 
   private GameState()
   {
-    actionInvokerDelegate = new ActionInvokerDelegate();
   }
 
   public static GameState instance()
@@ -29,10 +24,5 @@ public class GameState
       return;
     }
     instance = new GameState();
-  }
-
-  public ActionInvokerDelegate getActionInvokerDelegate()
-  {
-    return actionInvokerDelegate;
   }
 }
