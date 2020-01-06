727bf35dd6109d57916526927147377dcb5cb9d3
John Devenish <johnd4@discovery.co.za>
2020-01-06 21:42:58 +0200

    Better option selection

--- src/main/java/com/github/dagwud/woodlands/game/commands/natives/ReadOptionAction.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/natives/ReadOptionAction.java
@@ -12,19 +12,24 @@ import java.io.IOException;
 @SuppressWarnings("unused") // called at runtime via reflection
 public class ReadOptionAction extends NativeAction
 {
+  private static final String PARAMETER_NAME_PROMPT = "MessageToPrint";
   private static final String OUTPUT_CHOSEN_OPTION = "ChosenOption";
   private static final String PARAMETER_NAME_OPTIONS = "OptionsCSV";
 
   @Override
   public InvocationResults invoke(GameState gameState, CallDetails callDetails) throws IOException
   {
+    String chatId = gameState.getVariables().lookupVariableValue("chatId");
+
+    String prompt = gameState.getVariables().lookupVariableValue(PARAMETER_NAME_PROMPT);
     String optionsText = gameState.getVariables().lookupVariableValue(PARAMETER_NAME_OPTIONS);
     String[] options = optionsText.split(",");
+
+    String buttons = buildInlineKeyboard(options);
+    TelegramMessageSender.sendMessage(Integer.parseInt(chatId), prompt, buttons);
+
     Variables results = new Variables();
     results.put(OUTPUT_CHOSEN_OPTION, "${buffer}");
-    String chatId = gameState.getVariables().lookupVariableValue("chatId");
-    String buttons = buildInlineKeyboard(options);
-    TelegramMessageSender.sendMessage(Integer.parseInt(chatId), "Please make a choice", buttons);
     return new InvocationResults(results, ReturnMode.SUSPEND);
   }
 
