7592b955eee8dde8cb7c783b2e628bc770ee3f26
dagwud <johndevenish@gmail.com>
2020-01-06 14:39:34 +0200

    Update SetVarAction.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/natives/SetVarAction.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/natives/SetVarAction.java
@@ -16,6 +16,7 @@ public class SetVarAction extends NativeAction
     String varSet = gameState.getVariables().lookupVariableValue("VarSet");
     String varName = "__" + varSet + "." + gameState.getVariables().lookupVariableValue("VarName");
     String varValue = gameState.getVariables().lookupVariableValue("VarValue");
+    gameState.getVariables().setVariable(varName, varValue);
     Variables result = new Variables("setvarparams", new HashMap<>());
     result.put(varName, varValue);
     return new InvocationResults(result);
