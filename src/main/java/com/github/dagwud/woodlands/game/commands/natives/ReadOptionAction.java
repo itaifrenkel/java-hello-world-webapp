30f1508c1c460fba5f20859c685eb5505149e293
dagwud <johndevenish@gmail.com>
2020-01-06 08:15:48 +0200

    Update ReadOptionAction.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/natives/ReadOptionAction.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/natives/ReadOptionAction.java
@@ -43,8 +43,8 @@ public class ReadOptionAction extends NativeAction
       {
         b.append(",");
       }
-      b.append("[{text: '").append(option).append("', ")
-              .append("callback_data: '").append(option).append("}]");
+      b.append("[{text: \"").append(option).append("\", ")
+              .append("callback_data: \"").append(option).append("\"}]");
     }
     b.append("]}");
     /*
