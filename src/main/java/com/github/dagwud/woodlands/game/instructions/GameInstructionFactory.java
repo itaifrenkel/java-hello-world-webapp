e9e9f98439656a849c4dac86a70a9c82b6fab4bc
dagwud <johndevenish@gmail.com>
2020-01-06 13:59:13 +0200

    Update GameInstructionFactory.java

--- src/main/java/com/github/dagwud/woodlands/game/instructions/GameInstructionFactory.java
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/GameInstructionFactory.java
@@ -28,16 +28,32 @@ public class GameInstructionFactory
   public GameInstruction create(Update telegramUpdate, GameState gameState)
   {
     int chatId = telegramUpdate.message.chat.id;
-    if (telegramUpdate.message.text.equals("/new"))
+    String cmd = telegramUpdate.message.text;
+
+    if (cmd.equals("/new"))
     {
       return new CreateCharacterInstruction(chatId);
     }
-    if (telegramUpdate.message.text.equals("/help"))
+    if (cmd.equals("/help"))
     {
       return new ShowHelpInstruction();
     }
+    if (cmd.equals("/village"))
+    {
+      return new GoToVillageInstruction();
+    }
+    if (cmd.equals("/mountain"))
+    {
+      return new GoToMountainsInstruction();
+    }
+    if (cmd.equals("/woodlands"))
+    {
+      return new GoToWoodlandsInstruction();
+    }
+
     if (telegramUpdate.message.text.equals("/reset"))
     {
+//todo nuke this
       return new ResetServerInstruction();
     }
     return new SendMessageInstruction(chatId, "I'm not sure what you mean... perhaps try /help?");
