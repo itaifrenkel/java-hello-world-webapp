a92c54bc32e74b11134cec2f6e4df90af074a015
dagwud <johndevenish@gmail.com>
2020-01-06 13:52:33 +0200

    Create GoToWoodlandsInstruction.java

---
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/GoToWoodlandsInstruction.java
@@ -0,0 +1,28 @@
+package com.github.dagwud.woodlands.game.instructions;
+
+import com.github.dagwud.woodlands.game.GameState;
+import com.github.dagwud.woodlands.game.commands.invocation.ActionInvocationException;
+import com.github.dagwud.woodlands.game.commands.invocation.ActionInvocationPlanExecutor;
+import com.github.dagwud.woodlands.game.commands.invocation.ActionInvokerDelegate;
+import com.github.dagwud.woodlands.game.commands.invocation.CallDetails;
+import com.github.dagwud.woodlands.game.commands.invocation.plan.ActionInvocationPlanner;
+import com.github.dagwud.woodlands.game.commands.invocation.plan.InvocationPlan;
+import com.github.dagwud.woodlands.gson.game.ParamMappings;
+import com.github.dagwud.woodlands.telegram.TelegramMessageSender;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class GoToWoodlandsInstruction extends GameInstruction
+{
+  @Override
+  public void execute(GameState gameState) throws ActionInvocationException, IOException
+  {
+    Map<String, String> inputs = new HashMap<>();
+    inputs.put("Location", "The Woodlands");
+    CallDetails callDetails = new CallDetails(inputs, new ParamMappings());
+    InvocationPlan plan = ActionInvocationPlanner.plan("GoTo", gameState, callDetails);
+    ActionInvocationPlanExecutor.execute(plan);
+  }
+}
