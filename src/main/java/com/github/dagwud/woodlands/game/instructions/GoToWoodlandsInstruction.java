23d223e817f931fe3d0174749ccea07d57b95654
dagwud <johndevenish@gmail.com>
2020-01-06 14:11:52 +0200

    Update GoToWoodlandsInstruction.java

--- src/main/java/com/github/dagwud/woodlands/game/instructions/GoToWoodlandsInstruction.java
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/GoToWoodlandsInstruction.java
@@ -22,7 +22,7 @@ public class GoToWoodlandsInstruction extends GameInstruction
     Map<String, String> inputs = new HashMap<>();
     inputs.put("Location", "The Woodlands");
     CallDetails callDetails = new CallDetails(inputs, new ParamMappings());
-    InvocationPlan plan = ActionInvocationPlanner.plan("GoTo", gameState, callDetails);
+    InvocationPlan plan = ActionInvocationPlanner.plan("Goto", gameState, callDetails);
     ActionInvocationPlanExecutor.execute(plan);
   }
 }
