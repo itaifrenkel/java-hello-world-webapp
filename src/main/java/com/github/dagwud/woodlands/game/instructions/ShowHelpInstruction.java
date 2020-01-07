5d3b2ccf620cf473fa8bb9e531c85503428f44bf
dagwud <johndevenish@gmail.com>
2020-01-07 19:36:08 +0200

    Update ShowHelpInstruction.java

--- src/main/java/com/github/dagwud/woodlands/game/instructions/ShowHelpInstruction.java
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/ShowHelpInstruction.java
@@ -1,24 +1,9 @@
 package com.github.dagwud.woodlands.game.instructions;
 
-import com.github.dagwud.woodlands.game.GameState;
-import com.github.dagwud.woodlands.game.commands.invocation.ActionInvocationException;
-import com.github.dagwud.woodlands.game.commands.invocation.ActionInvocationPlanExecutor;
-import com.github.dagwud.woodlands.game.commands.invocation.CallDetails;
-import com.github.dagwud.woodlands.game.commands.invocation.Variables;
-import com.github.dagwud.woodlands.game.commands.invocation.plan.ActionInvocationPlanner;
-import com.github.dagwud.woodlands.game.commands.invocation.plan.InvocationPlan;
-
-public class ShowHelpInstruction extends GameInstruction
+public class ShowHelpInstruction extends RunProcInstruction
 {
   ShowHelpInstruction()
   {
-  }
-
-  @Override
-  public void execute(GameState gameState) throws ActionInvocationException
-  {
-    CallDetails callDetails = new CallDetails(new Variables(), new Variables());
-    InvocationPlan plan = ActionInvocationPlanner.plan("ShowHelp", gameState, callDetails);
-    ActionInvocationPlanExecutor.execute(plan);
+    super("ShowHelp");
   }
 }
