cff25a4b7f027462fe522402a09b37df5e102195
John Devenish <johnd4@discovery.co.za>
2020-01-07 22:18:15 +0200

    Basic item spawning

--- src/main/java/com/github/dagwud/woodlands/game/commands/natives/SpawnItemAction.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/natives/SpawnItemAction.java
@@ -1,29 +1,32 @@
-package com.github.dagwud.woodlands.game.instructions;
+package com.github.dagwud.woodlands.game.commands.natives;
 
 import com.github.dagwud.woodlands.game.GameState;
+import com.github.dagwud.woodlands.game.commands.invocation.CallDetails;
+import com.github.dagwud.woodlands.game.commands.invocation.InvocationResults;
+import com.github.dagwud.woodlands.game.commands.invocation.Variables;
 import com.github.dagwud.woodlands.game.items.ItemsCacheFactory;
 import com.github.dagwud.woodlands.gson.game.Weapon;
-import com.github.dagwud.woodlands.telegram.TelegramMessageSender;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 
-public class DebugSpawnItemInstruction extends GameInstruction
+@SuppressWarnings("unused") // called at runtime via reflection
+public class SpawnItemAction extends NativeAction
 {
   private static final String MELEE_ICON = "\u2694"; // crossed swords
   private static final String RANGED_ICON = "\ud83c\udff9"; // bow and arrow
 
   @Override
-  public void execute(GameState gameState) throws IOException
+  public InvocationResults invoke(GameState gameState, CallDetails callDetails)
   {
     String chatId = gameState.getVariables().lookupVariableValue("chatId");
-
     List<Weapon> allWeapons = ItemsCacheFactory.instance().getItems().getWeapons();
     int rand = (int) (Math.random() * allWeapons.size());
     Weapon chosenWeapon = allWeapons.get(rand);
     String weaponText = buildWeaponText(chosenWeapon);
-    TelegramMessageSender.sendMessage(Integer.parseInt(chatId), "You got a " + weaponText);
+
+    Variables result = new Variables();
+    result.put("SpawnedItem", weaponText);
+    return new InvocationResults(result);
   }
 
   private String buildWeaponText(Weapon chosenWeapon)
