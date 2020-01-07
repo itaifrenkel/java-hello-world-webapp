2c447e3ae4c285ce571cf577be5f82f4992c3893
dagwud <johndevenish@gmail.com>
2020-01-07 20:37:04 +0200

    Update Step.java

--- src/main/java/com/github/dagwud/woodlands/gson/game/Step.java
+++ src/main/java/com/github/dagwud/woodlands/gson/game/Step.java
@@ -3,6 +3,8 @@ package com.github.dagwud.woodlands.gson.game;
 import com.github.dagwud.woodlands.game.commands.invocation.Variables;
 import com.google.gson.annotations.SerializedName;
 
+import java.math.BigDecimal;
+
 public class Step
 {
   @SerializedName(value = "ProcName")
@@ -13,6 +15,23 @@ public class Step
 
   @SerializedName(value = "OutputMappings")
   public Variables outputMappings;
+
+  @SerializedName(value = "Chance")
+  public String chance;
+
+  public BigDecimal determineChance()
+  {
+    if (chance == null)
+    {
+      return BigDecimal.ONE;
+    }
+    String c = chance.trim(c);
+    if (c.endsWith("%"))
+    {
+      return new BigDecimal(c.substring(0, c.length() - 1)).divide(BigDeciml.HUNDRED);
+    }
+    throw new IllegalArgumentException("Unrecognizable chance format: \"" + c + "\"");
+  }
 
   @Override
   public String toString()
