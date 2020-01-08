6e681abb005fc7455ab32fbfc1c7a83cf0e34f7d
dagwud <johndevenish@gmail.com>
2020-01-08 19:10:10 +0200

    Update Weapon.java

--- src/main/java/com/github/dagwud/woodlands/gson/game/Weapon.java
+++ src/main/java/com/github/dagwud/woodlands/gson/game/Weapon.java
@@ -2,6 +2,9 @@ package com.github.dagwud.woodlands.gson.game;
 
 public class Weapon
 {
+  private static final String MELEE_ICON = "\u2694"; // crossed swords
+  private static final String RANGED_ICON = "\ud83c\udff9"; // bow and arrow
+
   public String name;
 
   public Damage damage;
@@ -11,4 +14,13 @@ public class Weapon
   public String customIcon;
 
   public boolean preventSpawning;
+
+  public String getIcon()
+  {
+    if (customIcon != null)
+    {
+      return customIcon;
+    }
+    return ranged ? RANGED_ICON : MELEE_ICON;
+  }
 }
