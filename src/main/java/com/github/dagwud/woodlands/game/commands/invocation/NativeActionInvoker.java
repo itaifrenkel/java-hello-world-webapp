5f532a7a68f1f2b03491c049216a883c1abe55bf
John Devenish <johnd4@discovery.co.za>
2020-01-12 08:48:53 +0200

    Cleaning up

--- src/main/java/com/github/dagwud/woodlands/game/commands/invocation/NativeActionInvoker.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/invocation/NativeActionInvoker.java
@@ -3,23 +3,51 @@ package com.github.dagwud.woodlands.game.commands.invocation;
 import com.github.dagwud.woodlands.game.GameState;
 import com.github.dagwud.woodlands.game.commands.natives.NativeAction;
 
-class NativeActionInvoker extends ActionInvoker
+import java.util.Map;
+
+class NativeActionInvoker
 {
+  final CallDetails callDetails;
   private final NativeAction nativeAction;
 
   NativeActionInvoker(NativeAction action, CallDetails callDetails)
   {
-    super(callDetails);
+    this.callDetails = callDetails;
     this.nativeAction = action;
   }
 
-  @Override
+  private static void mapResults(Variables results, VariableStack callContext, Variables outputMappings)
+  {
+    for (Map.Entry<String, String> outputMapping : outputMappings.entrySet()) // todo is it necessary to have OM on context now?
+    {
+      String outputName = outputMapping.getKey();
+      String mapToVariableName = outputMapping.getValue();
+      String outputValue = results.get(outputName);
+      //todo null handling of outputName==null - mapping refers to an invalid output
+      if (outputValue != null && outputValue.startsWith("$"))
+      {
+        outputValue = ValueResolver.resolve(outputValue, callContext);
+      }
+      callContext.setValue(mapToVariableName, outputValue, -1);
+    }
+
+    if (null != results)
+    {
+      for (Map.Entry<String, String> result : ((Map<String, String>) results).entrySet())
+      {
+        if (VariableStack.isGlobalVariable(result.getKey()))
+        {
+          callContext.setValue(result.getKey(), result.getValue());
+        }
+      }
+    }
+  }
+
   String getActionName()
   {
     return nativeAction.getClass().getSimpleName() + " (native)";
   }
 
-  @Override
   InvocationResults doInvoke(GameState gameState, Variables outputMappings) throws ActionInvocationException
   {
     try
@@ -38,5 +66,31 @@ class NativeActionInvoker extends ActionInvoker
     return "NativeActionInvoker{" +
             "nativeAction=" + nativeAction.getClass().getSimpleName() +
             '}';
+  }
+
+  final InvocationResults invoke(GameState gameState) throws ActionInvocationException
+  {
+    if (!this.getActionName().equals("PushVariablesAction"))
+    {
+//      System.out.println(getActionName() + " before call: \n" + gameState.getVariables());
+    }
+
+    InvocationResults results = doInvoke(gameState, callDetails.getOutputMappings());
+    if (results.getReturnMode() != ReturnMode.SUSPEND)
+    {
+      complete(gameState, results, callDetails.getOutputMappings());
+    }
+
+    return results;
+  }
+
+  void complete(GameState gameState, InvocationResults results, Variables outputMappings)
+  {
+    Variables resultVariables = results.getVariables();
+    mapResults(resultVariables, gameState.getVariables(), outputMappings);
+    if (resultVariables != null && !resultVariables.isEmpty())
+    {
+//      System.out.println(getActionName() + " after call: \n" + gameState.getVariables());
+    }
   }
 }
