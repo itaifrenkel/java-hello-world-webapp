0df3925322b16371a0a73ea7ed6f92946d2b13d5
dagwud <johndevenish@gmail.com>
2020-01-17 09:57:09 +0200

    Added missing menus for additional locations

--- src/main/java/com/github/dagwud/woodlands/game/instructions/MoveToLocationCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/MoveToLocationCmd.java
@@ -36,17 +36,36 @@ public class MoveToLocationCmd extends AbstractCmd
 
   private void showMenuForLocation(ELocation location)
   {
-    if (location == ELocation.INN)
-    {
-      String[] options = {"Leave Items", "Retrieve Items", "Short Rest", "Village Square"};
-      ShowMenuCmd cmd = new ShowMenuCmd(chatId, "This is the inn", options);
-      CommandDelegate.execute(cmd);
-    }
-    else if (location == ELocation.VILLAGE_SQUARE)
+    if (location == ELocation.VILLAGE_SQUARE)
     {
       String[] options = {"The Inn", "The Tavern", "The Mountain", "The Woodlands"};
       ShowMenuCmd cmd = new ShowMenuCmd(chatId, "The bustle of the village square brings a reassuring comfort to weary adventurers - the only danger here is the prices charged by the local bartender.\n\nThe square is dominated on the one side by the local tavern, from which you hear loud music and occasional raised voices.\n\nFacing it, within stumbling range, is the Inn. The innkeeper is a friend, and has offered to look after any items you may wish to leave behind while you're out on your adventures.\n\nTo the north lies a path leading up to The Mountain, and another leading out to the Woodlands.", options);
       CommandDelegate.execute(cmd);
     }
+    else if (location == ELocation.INN)
+    {
+      String[] options = {"Leave Items", "Retrieve Items", "Short Rest", "Village Square"};
+      ShowMenuCmd cmd = new ShowMenuCmd(chatId, "This is the inn", options);
+      CommandDelegate.execute(cmd);
+    }
+    else if (location == ELocation.TAVERN)
+    {
+      String[] options = {"Buy Drinks", "Village Square", "The Mountain"};
+      ShowMenuCmd cmd = new ShowMenuCmd(chat, "This is the Tavern", options);
+      CommandDelegate.execute(cmd);
+    }
+    else if (location == ELocation.MOUNTAIN)
+    {
+      String[] options = {"The Village"};
+      ShowMenuCmd cmd = new ShowMenuCmd(chat, "This is the Mountain", options);
+      CommandDelegate.execute(cmd);
+    }
+    else if (location == ELocation.WOODLANDS)
+    {
+      String[] options = {"The Gorge", "The Village"};
+      ShowMenuCmd cmd = new ShowMenuCmd(chat, "This is the Woodlands", options);
+      CommandDelegate.execute(cmd);
+    }
+
   }
 }
