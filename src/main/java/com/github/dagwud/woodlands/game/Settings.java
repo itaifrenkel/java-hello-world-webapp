8f3e8d639caebffa34b76c1dce0b2603422763ad
John Devenish <johnd4@discovery.co.za>
2020-01-19 22:44:24 +0200

    Slower battles for testing on Telegram

--- src/main/java/com/github/dagwud/woodlands/game/Settings.java
+++ src/main/java/com/github/dagwud/woodlands/game/Settings.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class Settings
 {
-  public static final int DELAY_BETWEEN_ENCOUNTERS_MS = 3000;
-  public static final int DELAY_BETWEEN_ROUNDS_MS = 1000;
+  public static final int DELAY_BETWEEN_ENCOUNTERS_MS = 15000;
+  public static final int DELAY_BETWEEN_ROUNDS_MS = 3000;
   public static final BigDecimal PERCENT_CHANGE_OF_ENCOUNTER = new BigDecimal("75");
 }
