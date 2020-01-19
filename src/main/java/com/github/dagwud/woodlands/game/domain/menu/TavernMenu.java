0b82e911addd6fa615ecd915047a7f7e78692a4d
dagwud <johndevenish@gmail.com>
2020-01-19 02:32:42 +0200

    Update TavernMenu.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
@@ -11,7 +11,7 @@ public class TavernMenu extends GameMenu
     "The Raven is a friendly place, where all are welcome as long as they pay for their drinks. A burly guard with a dark oversized coat stands watch, and the magic-imbued rings on a chain around his neck clearly signal this is not a man to be trifled with. \n" +
     "Apart from the guard, who looks permanently annoyed, the patrons of The Raven are all smiles - with the largest being the one plastered over the face of the barman.\n" +
     "\n" +
-    “Long journey?” he asks jovially, though something about his demeanor suggests he’s not that interested in your journey so much as how many coins are in your pocket");
+    "\"Long journey?\" he asks jovially, though something about his demeanor suggests he’s not that interested in your journey so much as how many coins are in your pocket";
 
     setPrompt("This is the tavern");
     setOptions(new String[]{"Buy Drinks", "Village Square"});
