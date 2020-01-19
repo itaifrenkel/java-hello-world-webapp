f7a748fcc1175591055710ccb3e2a0b9884857a1
dagwud <johndevenish@gmail.com>
2020-01-19 02:13:18 +0200

    Update TavernMenu.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
@@ -6,7 +6,7 @@ public class TavernMenu extends GameMenu
 {
   public TavernMenu()
   {
-    String message Aaron The smell of spilled beer permeates the local tavern, and the floor crunches slightly underfoot as you walk across the dirt from dozens of well-worn shoes.\n" +
+    String message = "The smell of spilled beer permeates the local tavern, and the floor crunches slightly underfoot as you walk across the dirt from dozens of well-worn shoes.\n" +
     "\n" +
     "The Raven is a friendly place, where all are welcome as long as they pay for their drinks. A burly guard with a dark oversized coat stands watch, and the magic-imbued rings on a chain around his neck clearly signal this is not a man to be trifled with. \n" +
     "Apart from the guard, who looks permanently annoyed, the patrons of The Raven are all smiles - with the largest being the one plastered over the face of the barman.\n"
