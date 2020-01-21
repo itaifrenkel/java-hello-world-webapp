15035f3511579c258e9c974c08826720072e671e
John Devenish <johnd4@discovery.co.za>
2020-01-22 00:26:35 +0200

    Format

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/LeaveItemsCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/LeaveItemsCmd.java
@@ -7,32 +7,32 @@ import com.github.dagwud.woodlands.game.domain.GameCharacter;
 
 public class LeaveItemsCmd extends AbstractCmd
 {
-    private final GameCharacter character;
+  private final GameCharacter character;
 
-    public LeaveItemsCmd(GameCharacter character)
+  public LeaveItemsCmd(GameCharacter character)
+  {
+    this.character = character;
+  }
+
+  @Override
+  public void execute()
+  {
+    StringBuilder response = new StringBuilder();
+    if (character.getCarrying().getCarriedLeft() != null)
     {
-        this.character = character;
+      response.append("You drop a ").append(character.getCarrying().getCarriedLeft().name).append(" from your left hand. ");
+      character.getCarrying().setCarriedLeft(null);
     }
 
-    @Override
-    public void execute()
+    if (character.getCarrying().getCarriedRight() != null)
     {
-        StringBuilder response = new StringBuilder();
-        if (character.getCarrying().getCarriedLeft() != null)
-        {
-            response.append("You drop a ").append(character.getCarrying().getCarriedLeft().name).append(" from your left hand. ");
-            character.getCarrying().setCarriedLeft(null);
-        }
-
-        if (character.getCarrying().getCarriedRight() != null)
-        {
-            response.append("You drop a ").append(character.getCarrying().getCarriedRight().name).append(" from your right hand. ");
-            character.getCarrying().setCarriedRight(null);
-        }
-
-        if (response.length() != 0)
-        {
-            CommandDelegate.execute(new SendMessageCmd(character.getPlayedBy().getChatId(), response.toString()));
-        }
+      response.append("You drop a ").append(character.getCarrying().getCarriedRight().name).append(" from your right hand. ");
+      character.getCarrying().setCarriedRight(null);
     }
+
+    if (response.length() != 0)
+    {
+      CommandDelegate.execute(new SendMessageCmd(character.getPlayedBy().getChatId(), response.toString()));
+    }
+  }
 }
