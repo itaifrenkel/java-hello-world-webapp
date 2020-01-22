85cf445237259742fd72f2132f42e7bf2517ce1b
dagwud <johndevenish@gmail.com>
2020-01-22 13:40:49 +0200

    Create DefeatCreatureCmd.java

---
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/mountain/DefeatCreatureCmd.java
@@ -0,0 +1,13 @@
+package com.github.dagwud.woodlands.game.commands.locations.mountain;
+
+public class DefeatCreatureCmd extends AbstractCmd
+{
+  public DefeatCreatureCmd()
+  {
+  }
+
+  @Override
+  public void execute()
+  {
+  }
+}
