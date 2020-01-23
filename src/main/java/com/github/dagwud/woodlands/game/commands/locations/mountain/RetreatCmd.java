ee00597a79f71400ec370469bc21a3e0f0065231
dagwud <johndevenish@gmail.com>
2020-01-23 11:28:53 +0200

    Better retreat

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/mountain/RetreatCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/mountain/RetreatCmd.java
@@ -6,6 +6,7 @@ import com.github.dagwud.woodlands.game.commands.core.SendPartyMessageCmd;
 import com.github.dagwud.woodlands.game.commands.locations.MoveToLocationCmd;
 import com.github.dagwud.woodlands.game.domain.ELocation;
 import com.github.dagwud.woodlands.game.domain.GameCharacter;
+import com.github.dagwud.woodlands.game.domain.WoodlandsRuntimeException;
 
 public class RetreatCmd extends AbstractCmd
 {
@@ -26,7 +27,8 @@ public class RetreatCmd extends AbstractCmd
     EndEncounterCmd end = new EndEncounterCmd(inDanger.getParty().getActiveEncounter());
     CommandDelegate.execute(end);
 
-    MoveToLocationCmd cmd = new MoveToLocationCmd(inDanger, ELocation.VILLAGE_SQUARE);
+    GameCharacter retreater = findActiveCharacter(inDanger.getParty());
+    MoveToLocationCmd cmd = new MoveToLocationCmd(retreater, ELocation.VILLAGE_SQUARE);
     CommandDelegate.execute(cmd);
   }
 
@@ -37,5 +39,17 @@ public class RetreatCmd extends AbstractCmd
       return "You're getting dangerously hurt, and decide to fall back to the village";
     }
     return inDanger.getName() + " sounds the retreat; " + inDanger.getParty().getName() + " falls back to The Village";
+  }
+
+  private GameCharacter findActiveCharacter(Party party)
+  {
+    for (GameCharacter member : party.getMembers())
+    {
+      if (member.getStats().getState() == EStatus.ALIVE)
+      {
+        return member;
+      }
+    }
+    throw new WoodlandsRuntimeException("Nobody is alive to sound the retreat");
   }
 }
