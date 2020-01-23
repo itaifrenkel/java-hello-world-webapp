76a960b3a521864ee1283c43fc3e50b18ba1fa21
John Devenish <johnd4@discovery.co.za>
2020-01-23 12:24:43 +0200

    Fix error

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/mountain/RetreatCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/mountain/RetreatCmd.java
@@ -4,9 +4,7 @@ import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendPartyMessageCmd;
 import com.github.dagwud.woodlands.game.commands.locations.MoveToLocationCmd;
-import com.github.dagwud.woodlands.game.domain.ELocation;
-import com.github.dagwud.woodlands.game.domain.GameCharacter;
-import com.github.dagwud.woodlands.game.domain.WoodlandsRuntimeException;
+import com.github.dagwud.woodlands.game.domain.*;
 
 public class RetreatCmd extends AbstractCmd
 {
@@ -20,32 +18,37 @@ public class RetreatCmd extends AbstractCmd
   @Override
   public void execute()
   {
-    String message = buildRetreatMessage();
+    GameCharacter retreater = inDanger.getStats().getState() == EState.ALIVE ? inDanger : findActiveCharacter(inDanger.getParty());
+    String message = buildRetreatMessage(inDanger, retreater);
     SendPartyMessageCmd msg = new SendPartyMessageCmd(inDanger.getParty(), message);
     CommandDelegate.execute(msg);
 
     EndEncounterCmd end = new EndEncounterCmd(inDanger.getParty().getActiveEncounter());
     CommandDelegate.execute(end);
 
-    GameCharacter retreater = findActiveCharacter(inDanger.getParty());
     MoveToLocationCmd cmd = new MoveToLocationCmd(retreater, ELocation.VILLAGE_SQUARE);
     CommandDelegate.execute(cmd);
   }
 
-  private String buildRetreatMessage()
+  private String buildRetreatMessage(GameCharacter inDanger, GameCharacter retreater)
   {
-    if (inDanger.getParty().isPrivateParty())
+    Party party = inDanger.getParty();
+    if (party.isPrivateParty())
     {
       return "You're getting dangerously hurt, and decide to fall back to the village";
     }
-    return inDanger.getName() + " sounds the retreat; " + inDanger.getParty().getName() + " falls back to The Village";
+    if (inDanger == retreater)
+    {
+      return inDanger.getName() + " sounds the retreat; " + party.getName() + " falls back to The Village";
+    }
+    return inDanger.getName() + " is badly wounded so " + retreater.getName() + " sounds the retreat; " + party.getName() + " falls back to The Village";
   }
 
   private GameCharacter findActiveCharacter(Party party)
   {
     for (GameCharacter member : party.getMembers())
     {
-      if (member.getStats().getState() == EStatus.ALIVE)
+      if (member.getStats().getState() == EState.ALIVE)
       {
         return member;
       }
