717c4904fd86d7fdee26dab92f4e730124b154eb
dagwud <johndevenish@gmail.com>
2020-01-26 16:27:50 +0200

    Update Wizard.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/characters/Wizard.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/characters/Wizard.java
@@ -3,11 +3,13 @@ package com.github.dagwud.woodlands.game.domain.characters;
 import com.github.dagwud.woodlands.game.domain.ECharacterClass;
 import com.github.dagwud.woodlands.game.domain.GameCharacter;
 import com.github.dagwud.woodlands.game.domain.Player;
+import com.github.dagwud.woodlands.game.domain.characters.spells.AuraOfProtection
 
 class Wizard extends GameCharacter
 {
   Wizard(Player playedBy)
   {
     super(playedBy, ECharacterClass.WIZARD);
+    getSpellAbilities().register(new AuraOfProtection(this));
   }
 }
