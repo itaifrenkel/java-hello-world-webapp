8302d04f611f41a712ce6650071b8d4b7a32e08d
dagwud <johndevenish@gmail.com>
2020-01-26 07:12:04 +0200

    Update SwitchCharacterCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/character/SwitchCharacterCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/character/SwitchCharacterCmd.java
@@ -1,9 +1,9 @@
 package com.github.dagwud.woodlands.game.commands.character;
 
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
-import com.github.dagwud.woodlands.game.commands.CommandDelegate;
+import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
-import com.github.dagwud.woodlands.game.domain.character.EState;
+import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.GameCharacter;
 import com.github.dagwud.woodlands.game.domain.Player;
 
