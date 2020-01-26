15923a465687605b35a4534f2a96a054cef2f6c7
dagwud <johndevenish@gmail.com>
2020-01-26 17:37:38 +0200

    Update AuraOfProtection.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/characters/spells/AuraOfProtection.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/characters/spells/AuraOfProtection.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class AuraOfProtection extends BattleRoundSpell
 {
-  private static final int BUFF_AMOUNT = 20;
+  private static final int BUFF_AMOUNT = 4;
 
   private Map<GameCharacter, Integer> buffs;
 
