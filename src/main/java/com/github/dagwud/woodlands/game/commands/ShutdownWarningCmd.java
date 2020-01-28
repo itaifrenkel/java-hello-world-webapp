342255592e263b42384a1bfddc69bd3302c87551
John Devenish <johnd4@discovery.co.za>
2020-01-28 23:40:52 +0200

    Attempt at notifications on server shutdown - a bit more robust

--- src/main/java/com/github/dagwud/woodlands/game/commands/ShutdownWarningCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/ShutdownWarningCmd.java
@@ -11,22 +11,41 @@ import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.Player;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class ShutdownWarningCmd extends AbstractCmd
 {
   @Override
   public void execute()
   {
+    Collection<PlayerCharacter> toRetreat = new ArrayList<>();
     for (PlayerState playerState : GameStatesRegistry.allPlayerStates())
     {
       PlayerCharacter character = playerState.getPlayer().getActiveCharacter();
       if (shouldRetreat(character))
       {
-        SendMessageCmd msg = new SendMessageCmd(character.getPlayedBy().getChatId(), "You can feel changes coming in the breeze. Best you head back to the Village to take shelter");
-        CommandDelegate.execute(msg);
+        toRetreat.add(character);
+      }
+    }
 
+    for (PlayerCharacter character : toRetreat)
+    {
+      try
+      {
         MoveToLocationCmd move = new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE);
         CommandDelegate.execute(move);
       }
+      catch (Exception e)
+      {
+        // Do NOT let this stop us retreating characters
+      }
+    }
+
+    for (PlayerCharacter character : toRetreat)
+    {
+      SendMessageCmd msg = new SendMessageCmd(character.getPlayedBy().getChatId(), "You can feel changes coming in the breeze. Best you head back to the Village to take shelter");
+      CommandDelegate.execute(msg);
     }
   }
 
