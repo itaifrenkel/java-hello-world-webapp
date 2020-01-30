1f1b5eac123963dc8c3a58ce844c1912948a3ef3
dagwud <johndevenish@gmail.com>
2020-01-30 15:03:53 +0200

    Update DeathCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/battle/DeathCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/battle/DeathCmd.java
@@ -8,6 +8,7 @@ import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.Fighter;
 import com.github.dagwud.woodlands.game.domain.NonPlayerCharacter;
+import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 import com.github.dagwud.woodlands.game.domain.characters.spells.PartySpell;
 
 public class DeathCmd extends AbstractCmd
