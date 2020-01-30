9ddfc9610bd2ec4d0b0d1759faa543a676e76ae3
dagwud <johndevenish@gmail.com>
2020-01-30 17:02:17 +0200

    Update CastSpellCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/character/CastSpellCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/character/CastSpellCmd.java
@@ -41,7 +41,7 @@ public class CastSpellCmd extends AbstractCmd
     spell.cast();
     spell.setCast(true);
 
-    SendMessageCmd success = new SendMessageCmd(((PlayerCharacter)spell.getCaster().getPlayedBy()).getChatId(), "You cast " + spell.buildSpellDescription());
+    SendMessageCmd success = new SendMessageCmd(((PlayerCharacter)spell.getCaster()).getPlayedBy().getChatId(), "You cast " + spell.buildSpellDescription());
     CommandDelegate.execute(success);
   }
 }
