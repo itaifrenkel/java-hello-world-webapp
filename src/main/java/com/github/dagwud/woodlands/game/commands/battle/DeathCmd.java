811e799bf5520350afee733aa0da1ddca825ca0d
dagwud <johndevenish@gmail.com>
2020-01-30 10:48:47 +0200

    Update DeathCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/battle/DeathCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/battle/DeathCmd.java
@@ -4,8 +4,10 @@ import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.commands.character.ExpireSpellCmd;
 import com.github.dagwud.woodlands.game.commands.character.ExpireSpellsCmd;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.Fighter;
+import com.github.dagwud.woodlands.game.domain.NonPlayerCharacter;
 import com.github.dagwud.woodlands.game.domain.characters.spells.PartySpell;
 
 public class DeathCmd extends AbstractCmd
@@ -23,5 +25,10 @@ public class DeathCmd extends AbstractCmd
     target.getStats().setState(EState.DEAD);
     ExpireSpellsCmd expireAll = new ExpireSpellsCmd(target.getSpellAbilities().getPartySpells());
     CommandDelegate.execute(expireAll);
+
+    if (target instanceof NonPlayerCharacter)
+    {
+      PlayerCharacter owner = ((NonPlayerCharacter)target).getOwnedBy();
+      CommandDelegate.execute(new SendMessageCmd(owner.getChatId(), target.getName() + " has died");
   }
 }
