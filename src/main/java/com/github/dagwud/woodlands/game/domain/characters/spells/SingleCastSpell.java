f0729f2abeb5fd92797bd42e7d07ba00c55770a0
dagwud <johndevenish@gmail.com>
2020-02-02 23:35:24 +0200

    Update SingleCastSpell.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/characters/spells/SingleCastSpell.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/characters/spells/SingleCastSpell.java
@@ -6,20 +6,9 @@ public abstract class SingleCastSpell extends Spell
 {
   private static final long serialVersionUID = 1L;
 
-  private DamageInflicted damageInflicted;
-
   SingleCastSpell(String spellName, Fighter caster)
   {
     super(spellName, caster);
   }
 
-  public DamageInflicted getDamageInflicted()
-  {
-    return damageInflicted;
-  }
-
-  public void setDamageInflicted(DamageInflicted damageInflicted)
-  {
-    this.damageInflicted = damageInflicted;
-  }
 }
