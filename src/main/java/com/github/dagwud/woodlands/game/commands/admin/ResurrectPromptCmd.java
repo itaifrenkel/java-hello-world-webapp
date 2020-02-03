e397ee343c9cf77491b9712800236a1d27d098b8
dagwud <johndevenish@gmail.com>
2020-02-03 21:01:42 +0200

    Update ResurrectPromptCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/admin/ResurrectPromptCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/ResurrectPromptCmd.java
@@ -3,7 +3,7 @@ package com.github.dagwud.woodlands.game.commands.admin;
 import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.GameStatesRegistry;
 import com.github.dagwud.woodlands.game.PartyRegistry;
-import com.github.dagwud.woodlands.game.commands.battle.FullHealCmd;
+import com.github.dagwud.woodlands.game.commands.FullHealCmd;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 import com.github.dagwud.woodlands.game.commands.core.SuspendableCmd;
