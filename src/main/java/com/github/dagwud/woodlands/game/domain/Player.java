a2d3123ab3c8cabb03946b4a2f87cad1f3cc0980
dagwud <johndevenish@gmail.com>
2020-02-04 08:57:43 +0200

    Update Player.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/Player.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/Player.java
@@ -49,6 +49,9 @@ public class Player implements Serializable
 
   public List<PlayerCharacter> getAllCharacters()
   {
+    if (activeCharacter == null) System.out.println(chatId + " has null active character");
+
+
     List<PlayerCharacter> all = new ArrayList<>(getInactiveCharacters().size() + 1);
     all.add(getActiveCharacter());
     all.addAll(getInactiveCharacters());
