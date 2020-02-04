443b38d573ad675b44da2cd4cf57b4b1c5f37a66
John Devenish <johnd4@discovery.co.za>
2020-02-04 07:27:40 +0200

    Admin command authorizations

--- src/main/java/com/github/dagwud/woodlands/game/commands/prerequisites/IsAdminPrerequisite.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/prerequisites/IsAdminPrerequisite.java
@@ -1,7 +1,9 @@
 package com.github.dagwud.woodlands.game.commands.prerequisites;
 
+import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.Settings;
 import com.github.dagwud.woodlands.game.commands.core.CommandPrerequisite;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 
 public class IsAdminPrerequisite implements CommandPrerequisite
 {
@@ -15,6 +17,12 @@ public class IsAdminPrerequisite implements CommandPrerequisite
   @Override
   public boolean verify()
   {
-    return chatId == Settings.ADMIN_CHAT;
+    if (chatId != Settings.ADMIN_CHAT)
+    {
+      SendMessageCmd notAdmin = new SendMessageCmd(chatId, "You're not an admin. Go away.");
+      CommandDelegate.execute(notAdmin);
+      return false;
+    }
+    return true;
   }
 }
