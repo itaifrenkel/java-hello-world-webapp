d3e2222c288dec67bb30c560fbc22bed32b0e907
dagwud <johndevenish@gmail.com>
2020-02-06 17:17:07 +0200

    Chris protection... no fun being stuck at the village

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/DoShortRestCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/DoShortRestCmd.java
@@ -1,11 +1,15 @@
 package com.github.dagwud.woodlands.game.commands.locations.village;
 
 import com.github.dagwud.woodlands.game.CommandDelegate;
+
+import com.github.dagwud.woodlands.game.commands.MoveToLocationCmd;
 import com.github.dagwud.woodlands.game.commands.RecoverHitPointsCmd;
 import com.github.dagwud.woodlands.game.commands.RecoverManaCmd;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
+import com.github.dagwud.woodlands.game.domain.ELocation;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
+
 
 public class DoShortRestCmd extends AbstractCmd
 {
@@ -41,6 +45,9 @@ public class DoShortRestCmd extends AbstractCmd
             "Now at ❤" + character.getStats().getHitPoints() + "/" + character.getStats().getMaxHitPoints() +
             ", ✨" + character.getStats().getMana() + "/" + character.getStats().getMaxMana());
     CommandDelegate.execute(echo);
+
+    MoveToLocationCmd chrisProtector = new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE);
+    CommandDelegate.execute(chrisProtector);
   }
 
   private void soberUp()
