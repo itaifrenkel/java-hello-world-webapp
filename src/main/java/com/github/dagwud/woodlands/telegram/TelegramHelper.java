8c73cbd66e42c6584f05d5a1e91a9fc89a346f20
dagwud <johndevenish@gmail.com>
2020-02-09 15:39:44 +0200

    Update TelegramHelper.java

--- src/main/java/com/github/dagwud/woodlands/telegram/TelegramHelper.java
+++ src/main/java/com/github/dagwud/woodlands/telegram/TelegramHelper.java
@@ -63,7 +63,7 @@ abstract class TelegramHelper
     Map<String, String> params = new HashMap<>();
     params.put("chat_id", String.valueOf(chatId));
     params.put("text", message);
-    params.put("parse_mode", "Markdown");
+    params.put("parse_mode", "MarkdownV2");
     if (null != replyMarkup)
     {
       params.put("reply_markup", replyMarkup);
