0db81b7acb4e93cf8bd26fb49dc36609727a0d7e
dagwud <johndevenish@gmail.com>
2020-02-11 12:26:56 +0200

    Worn items

--- src/main/java/com/github/dagwud/woodlands/game/instructions/CommandFactory.java
+++ src/main/java/com/github/dagwud/woodlands/game/instructions/CommandFactory.java
@@ -66,7 +66,7 @@ public class CommandFactory
       return by.build(playerState.getActiveCharacter(), chatId);
     }
 
-    if (cmd.matches("/d[LR0-9]+"))
+    if (cmd.matches("/d[LRW0-9]+"))
     {
       // Drop:
       String dropIndex = cmd.substring("/d".length());
