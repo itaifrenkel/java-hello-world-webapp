065d41ede0886f5738f5651546e7c30f11ce4b1b
dagwud <johndevenish@gmail.com>
2020-02-12 16:34:23 +0200

    Create AdminShowCharacterInfoCmd.java

---
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/AdminShowCharacterInfoCmd.java
@@ -0,0 +1,75 @@
+package com.github.dagwud.woodlands.game.commands.admin;
+
+import com.github.dagwud.woodlands.game.CommandDelegate;
+import com.github.dagwud.woodlands.game.PartyRegistry;
+import com.github.dagwud.woodlands.game.Settings;
+import com.github.dagwud.woodlands.game.commands.ShowCharacterInfoCmd;
+import com.github.dagwud.woodlands.game.commands.battle.DeathCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
+import com.github.dagwud.woodlands.game.commands.core.SuspendableCmd;
+import com.github.dagwud.woodlands.game.domain.GameCharacter;
+import com.github.dagwud.woodlands.game.domain.Party;
+import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
+
+public class AdminShowCharacterInfoCmd extends SuspendableCmd
+{
+  private static final long serialVersionUID = 1L;
+  private final int chatId;
+
+  public KillPromptCmd(int chatId, PlayerCharacter character)
+  {
+    super(character.getPlayedBy().getPlayerState(), 2);
+    this.chatId = chatId;
+  }
+
+  @Override
+  protected void executePart(int phaseToExecute, String capturedInput)
+  {
+    switch (phaseToExecute)
+    {
+      case 0:
+        promptForCharacter();
+        break;
+      case 1:
+        show(capturedInput);
+        break;
+    }
+  }
+
+  private void promptForCharacter()
+  {
+    SendMessageCmd cmd = new SendMessageCmd(chatId, "Please enter the character name");
+    CommandDelegate.execute(cmd);
+  }
+
+  private void show(String name)
+  {
+    if (getPlayerState().getPlayer().getChatId() != Settings.ADMIN_CHAT)
+    {
+      SendMessageCmd notAdmin = new SendMessageCmd(getPlayerState().getPlayer().getChatId(), "You're not an admin. Go away.");
+      CommandDelegate.execute(notAdmin);
+      return;
+    }
+    StringBuilder done = new StringBuilder("Killing off...\n");
+    for (Party party : PartyRegistry.listAllParties())
+    {
+      for (GameCharacter gameCharacter : party.getActiveMembers())
+      {
+        if (gameCharacter instanceof PlayerCharacter)
+        {
+          PlayerCharacter character = (PlayerCharacter)gameCharacter;
+          if (character.getName().equalsIgnoreCase(name))
+          {
+            ShowCharacterInfoCmd cmd = new ShowCharacterInfoCmd(chatId, character);
+            CommandDelegate.execute(cmd);
+
+            done.append("\n\n");
+          }
+        }
+      }
+    }
+
+    SendMessageCmd cmd = new SendMessageCmd(chatId, done.toString());
+    CommandDelegate.execute(cmd);
+  }
+}
