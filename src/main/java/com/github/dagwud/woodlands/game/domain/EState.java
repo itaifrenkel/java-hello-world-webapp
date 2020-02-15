9c431cbbff897cc2b2c05ab1a230403dbb50ece7
dagwud <johndevenish@gmail.com>
2020-02-15 09:06:00 +0200

    Update EState.java

--- src/main/java/com/github/dagwud/woodlands/game/domain/EState.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/EState.java
@@ -2,8 +2,15 @@ package com.github.dagwud.woodlands.game.domain;
 
 public enum EState
 {
-  RESTING,
-  ALIVE,
-  UNCONSCIOUS,
-  DEAD
+  RESTING("🛏️"),
+  ALIVE(""),
+  UNCONSCIOUS("💔"),
+  DEAD("☠️");
+
+  private final String icon;
+
+  EState(String icon)
+  {
+    this.icon = icon;
+  }
 }
