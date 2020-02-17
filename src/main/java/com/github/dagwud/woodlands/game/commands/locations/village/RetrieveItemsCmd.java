083d7fbed3abd2c6bd508b60975c178a5bc5529e
dagwud <johndevenish@gmail.com>
2020-02-18 00:12:36 +0200

    #6 Retrieve items from innkeeper

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/RetrieveItemsCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/village/RetrieveItemsCmd.java
@@ -1,43 +1,78 @@
 package com.github.dagwud.woodlands.game.commands.locations.village;
 
 import com.github.dagwud.woodlands.game.CommandDelegate;
-import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
+import com.github.dagwud.woodlands.game.commands.core.ChoiceCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
-import com.github.dagwud.woodlands.game.commands.inventory.SpawnItemCmd;
+import com.github.dagwud.woodlands.game.commands.core.SuspendableCmd;
+import com.github.dagwud.woodlands.game.commands.prerequisites.AbleToActPrerequisite;
+import com.github.dagwud.woodlands.game.commands.inventory.DoGiveItemCmd;
+import com.github.dagwud.woodlands.game.commands.inventory.GiveItemCmd;
+import com.github.dagwud.woodlands.game.domain.CarriedItems;
 import com.github.dagwud.woodlands.game.domain.Item;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 
-public class RetrieveItemsCmd extends AbstractCmd
+import java.util.ArrayList;
+import java.util.List;
+
+public class RetrieveItemsCmd extends SuspendableCmd
 {
   private static final long serialVersionUID = 1L;
 
-  private final PlayerCharacter character;
-
   public RetrieveItemsCmd(PlayerCharacter character)
   {
-    this.character = character;
+    super(character.getPlayedBy().getPlayerState(), 2, new AbleToActPrerequisite(character));
   }
 
   @Override
-  public void execute()
+  public void executePart(int phaseToExecute, String capturedInput)
   {
-    if (!character.canCarryMore())
+    switch (phaseToExecute)
     {
-      CommandDelegate.execute(new SendMessageCmd(character.getPlayedBy().getChatId(), "You can't carry any more"));
-      return;
+      case 0:
+        String[] items = buildItemsList(getPlayerState().getActiveCharacter().getInnkeeper().getCarrying());
+        ChoiceCmd cmd = new ChoiceCmd(getPlayerState().getPlayer().getChatId(), "What would you like to leave with the Innkeeper?", items);
+        CommandDelegate.execute(cmd);
+        return;
+      case 1:
+        Item toRetrieve = findItem(capturedInput);
+        if (toRetrieve == null)
+        {
+          CommandDelegate.execute(new SendMessageCmd(getPlayerState().getPlayer().getChatId(), "\"Suit yourself\""));
+          return;
+        }
+        retrieveItem(toRetrieve);
+        CommandDelegate.execute(new SendMessageCmd(getPlayerState().getPlayer().getChatId(), "\"Sure, I'll hang on to that for you\""));
     }
-
-    Item chosenItem = chooseItem();
-
-    character.getCarrying().getCarriedInactive().add(chosenItem);
-    CommandDelegate.execute(new SendMessageCmd(character.getPlayedBy().getChatId(), "You pick up a " + chosenItem.getName() + " " + chosenItem.getIcon() + chosenItem.statsSummary(character)));
   }
 
-  private Item chooseItem()
+  private String[] buildItemsList(CarriedItems carrying)
   {
-    SpawnItemCmd cmd = new SpawnItemCmd(false);
-    CommandDelegate.execute(cmd);
-    return cmd.getSpawned();
+    List<String> items = new ArrayList<>();
+    for (Item item : carrying.getCarriedInactive())
+    {
+      items.add(item.getName());
+    }
+    return items.toArray(new String[0]);
   }
 
+  private Item findItem(String capturedInput)
+  {
+    for (Item item : getPlayerState().getPlayer().getActiveCharacter().getInnkeeper().getCarrying().getCarriedInactive())
+    {
+      if (item.getName().equalsIgnoreCase(capturedInput))
+      {
+        return item;
+      }
+    }
+    return null;
+  }
+
+  private void retrieveItem(Item toRetrieve)
+  {
+    PlayerCharacter character = getPlayerState().getPlayer().getActiveCharacter();
+    DoGiveItemCmd give = new DoGiveItemCmd(character.getInnkeeper(),
+            character.getInnkeeper().getCarrying().getCarriedInactive(),
+            character, toRetrieve);
+    CommandDelegate.execute(give);
+  }
 }
