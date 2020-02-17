61f893d3aa0abf16ea1e4090743f22a2633073bb
dagwud <johndevenish@gmail.com>
2020-02-17 14:25:52 +0200

    Patch for testing player

--- src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchCharacterCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchCharacterCmd.java
@@ -9,7 +9,7 @@ import com.github.dagwud.woodlands.game.commands.inventory.DropItemCmd;
 import com.github.dagwud.woodlands.game.domain.ECharacterClass;
 import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.ELocation;
-import com.github.dagwud.woodlands.game.commands.location.MoveToLocationCmd;
+import com.github.dagwud.woodlands.game.commands.locations.MoveToLocationCmd;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 import com.github.dagwud.woodlands.game.domain.characters.spells.HealingBlast;
 import com.github.dagwud.woodlands.game.domain.characters.spells.SingleCastSpell;
