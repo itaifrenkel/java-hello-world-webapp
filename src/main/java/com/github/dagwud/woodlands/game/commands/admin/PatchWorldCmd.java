555d121e502f020549acba71b366b8790afa26bf
John Devenish <johnd4@discovery.co.za>
2020-02-18 16:22:22 +0200

    Trying to fix schedule issues

--- src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchWorldCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchWorldCmd.java
@@ -1,9 +1,12 @@
 package com.github.dagwud.woodlands.game.commands.admin;
 
-import com.github.dagwud.woodlands.game.CommandDelegate;
-import com.github.dagwud.woodlands.game.GameStatesRegistry;
-import com.github.dagwud.woodlands.game.PlayerState;
+import com.github.dagwud.woodlands.game.*;
+import com.github.dagwud.woodlands.game.commands.character.PeriodicSoberUpCmd;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
+import com.github.dagwud.woodlands.game.commands.locations.MoveToLocationCmd;
+import com.github.dagwud.woodlands.game.domain.ELocation;
+import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 
 public class PatchWorldCmd extends AbstractCmd
@@ -22,6 +25,38 @@ public class PatchWorldCmd extends AbstractCmd
         patch(inactiveCharacter);
       }
     }
+
+    if (Scheduler.instance().count() == 0)
+    {
+      recreateSchedules();
+    }
+  }
+
+  private void recreateSchedules()
+  {
+    CommandDelegate.execute(new SendMessageCmd(Settings.ADMIN_CHAT, "Schedules appear to have been lost! Recreating them..."));
+    for (PlayerState playerState : GameStatesRegistry.allPlayerStates())
+    {
+      createSchedules(playerState.getActiveCharacter());
+      for (PlayerCharacter inactiveCharacter : playerState.getPlayer().getInactiveCharacters())
+      {
+        createSchedules(inactiveCharacter);
+      }
+
+      if (playerState.getActiveCharacter().isResting())
+      {
+        PlayerCharacter activeCharacter = playerState.getActiveCharacter();
+        activeCharacter.getStats().setState(EState.ALIVE);
+        CommandDelegate.execute(new MoveToLocationCmd(activeCharacter, ELocation.VILLAGE_SQUARE));
+        CommandDelegate.execute(new SendMessageCmd(Settings.ADMIN_CHAT, "Unrested " + activeCharacter.getName()));
+      }
+    }
+  }
+
+  private void createSchedules(PlayerCharacter character)
+  {
+    PeriodicSoberUpCmd periodicSoberUp = new PeriodicSoberUpCmd(character, character.getPlayedBy().getChatId());
+    CommandDelegate.execute(periodicSoberUp);
   }
 
   private void patch(PlayerCharacter character)
