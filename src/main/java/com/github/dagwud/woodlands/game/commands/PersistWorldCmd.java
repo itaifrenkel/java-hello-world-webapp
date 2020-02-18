63e7bb9d18c7c1fd581bcb3415768a79e51b62fe
John Devenish <johnd4@discovery.co.za>
2020-02-18 16:53:37 +0200

    Tell admin when save succeeded

--- src/main/java/com/github/dagwud/woodlands/game/commands/PersistWorldCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/PersistWorldCmd.java
@@ -3,7 +3,9 @@ package com.github.dagwud.woodlands.game.commands;
 import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.GameStatesRegistry;
 import com.github.dagwud.woodlands.game.PartyRegistry;
+import com.github.dagwud.woodlands.game.Settings;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 import com.github.dagwud.woodlands.game.log.Logger;
 
 public class PersistWorldCmd extends AbstractCmd
@@ -37,6 +39,7 @@ public class PersistWorldCmd extends AbstractCmd
     persist(gameState, filename);
 
     Logger.info("Successfully persisted world!");
+    CommandDelegate.execute(new SendMessageCmd(Settings.ADMIN_CHAT, "Persisted " + filename));
   }
 
   private void persist(Object object, String fileName)
