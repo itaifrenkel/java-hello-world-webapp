8dd47790106bd93a8c674fdc7e8c2419c5a3bba2
Evan Knowles <evanjamesknowles@gmail.com>
2020-02-19 19:10:23 +0200

    Closes #107 - only show celebrate when it's worth it.

--- src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/menu/TavernMenu.java
@@ -1,6 +1,11 @@
 package com.github.dagwud.woodlands.game.domain.menu;
 
+import com.github.dagwud.woodlands.game.PlayerState;
 import com.github.dagwud.woodlands.game.commands.ECommand;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TavernMenu extends GameMenu
 {
@@ -9,7 +14,21 @@ public class TavernMenu extends GameMenu
   public TavernMenu()
   {
     setPrompt("<i>This is the Tavern</i>");
-    setOptions(ECommand.BUY_DRINKS, ECommand.WAKE,
-            ECommand.UPGRADE, ECommand.VILLAGE_SQUARE);
+    setOptions(ECommand.BUY_DRINKS, ECommand.WAKE);
+  }
+
+  @Override
+  public String[] produceOptions(PlayerState playerState)
+  {
+    // Arrays.asList produces an immutable list
+    List<String> options = new ArrayList<>(Arrays.asList(super.produceOptions(playerState)));
+    if (playerState.getActiveCharacter().getStats().getAvailableStatsPointUpgrades() > 0)
+    {
+      options.add(ECommand.UPGRADE.toString());
+    }
+
+    options.add(ECommand.VILLAGE_SQUARE.toString());
+
+    return options.toArray(new String[0]);
   }
 }
