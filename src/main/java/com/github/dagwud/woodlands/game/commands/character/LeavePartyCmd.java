1a872113fb0792cfb9c780ee052794b1627dc7a1
dagwud <johndevenish@gmail.com>
2020-02-22 18:35:47 +0200

    #113

--- src/main/java/com/github/dagwud/woodlands/game/commands/character/LeavePartyCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/character/LeavePartyCmd.java
@@ -2,6 +2,7 @@ package com.github.dagwud.woodlands.game.commands.character;
 
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendPartyMessageCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendPartyAlertCmd;
 import com.github.dagwud.woodlands.game.commands.prerequisites.AbleToActPrerequisite;
 import com.github.dagwud.woodlands.game.domain.GameCharacter;
 import com.github.dagwud.woodlands.game.domain.Party;
@@ -39,6 +40,7 @@ public class LeavePartyCmd extends AbstractCmd
       CommandDelegate.execute(new SendPartyMessageCmd(party, character.getName() + " has left " + party.getName()));
     }
     party.removeMember(character);
+    CommandDelegate.execute(new SendPartyAlertCmd(party, joiner.getName() + " has left " + party.getName()));
   }
 
   @Override
