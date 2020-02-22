f59f703de225e904ac0c38a9dad9fc34fb195a30
dagwud <johndevenish@gmail.com>
2020-02-22 19:22:16 +0200

    #113

--- src/main/java/com/github/dagwud/woodlands/game/commands/core/SendPartyAlertCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/core/SendPartyAlertCmd.java
@@ -30,6 +30,7 @@ public class SendPartyAlertCmd extends AbstractCmd
     {
 System.out.println("send to " + party.getAlertChatId() + ": " + message);
      MessagingFactory.create().sender().sendMessage(party.getAlertChatId(), message);
+System.out.println("sent");
     }
     catch (Exception e)
     {
