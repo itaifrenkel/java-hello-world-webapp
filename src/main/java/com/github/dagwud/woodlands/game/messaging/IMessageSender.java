7ec465e3d040ac1bcd0e7df8e39e1c4eb392bb6a
dagwud <johndevenish@gmail.com>
2020-02-22 18:27:35 +0200

    #113

--- src/main/java/com/github/dagwud/woodlands/game/messaging/IMessageSender.java
+++ src/main/java/com/github/dagwud/woodlands/game/messaging/IMessageSender.java
@@ -3,6 +3,6 @@ package com.github.dagwud.woodlands.game.messaging;
 import java.io.IOException;
 
 public interface IMessageSender {
-    void sendMessage(int chatId, String message, String replyMarkup) throws IOException;
-    void sendMessage(int chatId, String message) throws IOException;
+    void sendMessage(long chatId, String message, String replyMarkup) throws IOException;
+    void sendMessage(long chatId, String message) throws IOException;
 }
