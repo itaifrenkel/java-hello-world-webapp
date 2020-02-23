85ebe9bd6431aa329f1c83f0210c21de75a40c62
dagwud <johndevenish@gmail.com>
2020-02-23 20:46:45 +0200

    #118

--- src/main/java/com/github/dagwud/woodlands/game/commands/RecoverHitPointsCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/RecoverHitPointsCmd.java
@@ -1,5 +1,6 @@
 package com.github.dagwud.woodlands.game.commands;
 
+import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.domain.EState;
 import com.github.dagwud.woodlands.game.domain.Fighter;
