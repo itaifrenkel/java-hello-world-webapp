6db566f73b5c1b81dafbc6274e9a112c4185c444
dagwud <johndevenish@gmail.com>
2020-02-26 19:16:49 +0200

    #115

--- src/main/java/com/github/dagwud/woodlands/game/commands/character/PeriodicSoberUpCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/character/PeriodicSoberUpCmd.java
@@ -8,13 +8,15 @@ import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 public class PeriodicSoberUpCmd extends PeriodicCmd<SoberUpCmd>
 {
   private static final long serialVersionUID = 1L;
-
+  
+  private final int chatId;
   private final PlayerCharacter character;
 
   public PeriodicSoberUpCmd(PlayerCharacter character, int chatId)
   {
     super(Settings.SOBER_UP_DELAY_MS);
     this.character = character;
+    this.chatId = chatId;
   }
 
   @Override
