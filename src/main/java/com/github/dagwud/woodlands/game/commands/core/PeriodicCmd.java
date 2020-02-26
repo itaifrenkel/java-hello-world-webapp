993c51835070911869e1b8e1a28499129976d1e2
dagwud <johndevenish@gmail.com>
2020-02-26 19:05:11 +0200

    #115

--- src/main/java/com/github/dagwud/woodlands/game/commands/core/PeriodicCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/core/PeriodicCmd.java
@@ -7,28 +7,24 @@ public abstract class PeriodicCmd<T extends AbstractCmd> extends AbstractCmd
   private static final long serialVersionUID = 1L;
 
   private final long runEveryMS;
-  private final T timerTickCmd;
 
-  public PeriodicCmd(long runEveryMS, T timerTickCmd)
+  public PeriodicCmd(long runEveryMS)
   {
     this.runEveryMS = runEveryMS;
-    this.timerTickCmd = timerTickCmd;
   }
 
   @Override
   public final void execute()
   {
-    if (null == timerTickCmd)
-    {
-      return;
-    }
-
     if (!shouldCancelPeriodicTimer())
     {
+      T timerTickCmd = createSingleRunCmd();
       CommandDelegate.execute(timerTickCmd);
       CommandDelegate.execute(new RunLaterCmd(runEveryMS, this));
     }
   }
 
   protected abstract boolean shouldCancelPeriodicTimer();
+
+  protected abstract T createSingleRunCmd();
 }
