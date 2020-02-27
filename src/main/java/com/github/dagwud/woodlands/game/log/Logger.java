4bd952f6bf638e6784b9af87d2cef99225494dc3
Evan Knowles <evanjamesknowles@gmail.com>
2020-02-27 16:19:55 +0200

    Fixed import

--- src/main/java/com/github/dagwud/woodlands/game/log/Logger.java
+++ src/main/java/com/github/dagwud/woodlands/game/log/Logger.java
@@ -1,6 +1,7 @@
 package com.github.dagwud.woodlands.game.log;
 
 import com.github.dagwud.woodlands.game.CommandDelegate;
+import com.github.dagwud.woodlands.game.Settings;
 import com.github.dagwud.woodlands.game.commands.core.SendAdminMessageCmd;
 
 import java.io.ByteArrayOutputStream;
