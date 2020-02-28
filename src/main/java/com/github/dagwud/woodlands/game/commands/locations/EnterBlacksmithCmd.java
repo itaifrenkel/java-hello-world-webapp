0cfa9d01fb7089cec8960616b14ba788c064adc9
dagwud <johndevenish@gmail.com>
2020-02-28 13:55:08 +0200

    Blacksmith is open for business

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/EnterBlacksmithCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/EnterBlacksmithCmd.java
@@ -22,12 +22,6 @@ public class EnterBlacksmithCmd extends AbstractCmd
   @Override
   public void execute()
   {
-    if (character.getPlayedBy().getChatId() != Settings.ADMIN_CHAT)
-    {
-      CommandDelegate.execute(new SendMessageCmd(character, "The door is locked. There's a hurriedly-written sign on the window: \"Opening Soon\""));
-      CommandDelegate.execute(new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE));
-    }
-
     if (character.getParty().getBlacksmith().isBusyCrafting())
     {
       CommandDelegate.execute(new SendMessageCmd(character, "The door to the Blacksmith's shop is locked; you can hear the sounds of clashing steel coming from his workshop. He must be busy with something."));
