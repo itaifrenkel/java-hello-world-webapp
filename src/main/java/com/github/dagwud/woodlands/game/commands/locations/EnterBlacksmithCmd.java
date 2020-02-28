a1b3e57331aca61393aa15fe6429ed2448be8efd
dagwud <johndevenish@gmail.com>
2020-02-28 10:01:29 +0200

    Blacksmith collection

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/EnterBlacksmithCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/EnterBlacksmithCmd.java
@@ -4,8 +4,10 @@ import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.Settings;
 import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
+import com.github.dagwud.woodlands.game.commands.inventory.DoGiveItemCmd;
 import com.github.dagwud.woodlands.game.domain.ELocation;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
+import com.github.dagwud.woodlands.gson.game.Weapon;
 
 public class EnterBlacksmithCmd extends AbstractCmd
 {
@@ -29,6 +31,13 @@ public class EnterBlacksmithCmd extends AbstractCmd
     {
       CommandDelegate.execute(new SendMessageCmd(character, "The door to the Blacksmith's shop is locked; you can hear the sounds of clashing steel coming from his workshop. He must be busy with something."));
       CommandDelegate.execute(new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE));
+    }
+
+    Weapon collected = character.getParty().getBlacksmith(). collectWeaponFor(character);
+    if (null != collected)
+    {
+      CommandDelegate.execute(new SendMessageCmd(character, "The Blacksmith smiles as you enter. \"Ah!\" he says, \"I've got something for you. I think you're gonna like it \""));
+      CommandDelegate.execute(new DoGiveItemCmd(blacksmith, character, collected));
     }
   }
 }
