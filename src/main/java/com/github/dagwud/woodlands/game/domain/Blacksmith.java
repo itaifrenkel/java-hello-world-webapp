0299a734c0c0a6a36f990f534bb76033187e6595
dagwud <johndevenish@gmail.com>
2020-02-28 09:52:29 +0200

    Blacksmith collection

--- src/main/java/com/github/dagwud/woodlands/game/domain/Blacksmith.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/Blacksmith.java
@@ -1,9 +1,15 @@
 package com.github.dagwud.woodlands.game.domain;
 
+import com.github.dagwud.woodlands.gson.game.Weapon;
+
+import java.util.Map;
+import java.util.HashMap;
+
 public class Blacksmith extends NonPlayerCharacter
 {
   private static final long serialVersionUID = 1L;
   private boolean busyCrafting;
+  private Map<PlayerCharacter, Weapon> readyForCollection;
 
   Blacksmith()
   {
@@ -19,5 +25,14 @@ public class Blacksmith extends NonPlayerCharacter
   public boolean isBusyCrafting()
   {
     return busyCrafting;
+  }
+
+  public Weapon collectWeaponFor(PlayerCharacter craftedFor)
+  {
+    if (readyForCollection == null)
+    {
+      readyForCollection = new HashMap<>();
+    }
+    return readyForCollection.remove(craftedFor);
   }
 }
