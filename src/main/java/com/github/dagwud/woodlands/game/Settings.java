08f6aa93a057c6d49d1deb5bc7f6ccd67c7d4b8b
John Devenish <johnd4@discovery.co.za>
2020-02-29 21:42:36 +0200

    Blacksmith takes longer to work

--- src/main/java/com/github/dagwud/woodlands/game/Settings.java
+++ src/main/java/com/github/dagwud/woodlands/game/Settings.java
@@ -24,7 +24,7 @@ public class Settings
   public static final long SHORT_REST_DURATION_MS = 60_000 * 5;
   public static final long LONG_REST_DURATION_MS = 60_000 * 60; // 1 hour
 
-  public static final long BLACKSMITH_CRAFTING_TIME_MS = 10000; //todo
+  public static final long BLACKSMITH_CRAFTING_TIME_MS = 78 * 60 * 1000; // 1 hour 18 minutes... because he's a little arbitrary
 
   public static final int MAX_UNCLAIMED_PARTY_ITEMS = 5;
 
