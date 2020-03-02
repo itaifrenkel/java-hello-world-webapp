d9b7ac2d8cf1fadab1fe3e5103091227d19723d2
Evan Knowles <evanjamesknowles@gmail.com>
2020-03-02 22:03:41 +0200

    Most achievements achievement - closes #133

--- src/main/java/com/github/dagwud/woodlands/game/commands/character/UnlockAchievementCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/character/UnlockAchievementCmd.java
@@ -5,6 +5,7 @@ import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
 import com.github.dagwud.woodlands.game.commands.core.SendPartyMessageCmd;
 import com.github.dagwud.woodlands.game.domain.EAchievement;
+import com.github.dagwud.woodlands.game.domain.GameCharacter;
 import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 
 import java.util.Set;
@@ -37,5 +38,48 @@ public class UnlockAchievementCmd extends AbstractCmd
 
     SendPartyMessageCmd sendPartyMessageCmd = new SendPartyMessageCmd(playerCharacter.getParty(), playerCharacter.getName() + " has unlocked achievement " + achievement.getAchievementName());
     CommandDelegate.execute(sendPartyMessageCmd);
+
+    checkForMostAchievementsAchievement();
+  }
+
+  private void checkForMostAchievementsAchievement()
+  {
+    if (achievement == EAchievement.ALL_THE_ACHIEVEMENTS)
+    {
+      return;
+    }
+
+    Set<EAchievement> achievements = playerCharacter.getStats().getAchievements();
+    if (achievements.contains(EAchievement.ALL_THE_ACHIEVEMENTS))
+    {
+      return;
+    }
+
+    long current = count(achievements);
+    for (GameCharacter activeMember : playerCharacter.getParty().getActiveMembers())
+    {
+      if (!(activeMember instanceof PlayerCharacter) || activeMember == playerCharacter)
+      {
+        continue;
+      }
+
+      long count = count(activeMember.getStats().getAchievements());
+      if (count >= current)
+      {
+        return;
+      }
+    }
+
+    for (GameCharacter activeMember : playerCharacter.getParty().getActiveMembers())
+    {
+      activeMember.getStats().getAchievements().remove(EAchievement.ALL_THE_ACHIEVEMENTS);
+    }
+
+    CommandDelegate.execute(new UnlockAchievementCmd(playerCharacter, EAchievement.ALL_THE_ACHIEVEMENTS));
+  }
+
+  private long count(Set<EAchievement> achievements)
+  {
+    return achievements.stream().filter(eAchievement -> eAchievement != EAchievement.ALL_THE_ACHIEVEMENTS).count();
   }
 }
