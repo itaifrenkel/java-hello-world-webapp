3a4bcad6a5535f99a4f8bf85072f4116fc246f63
dagwud <johndevenish@gmail.com>
2020-03-02 14:51:19 +0200

    Blacksmith cosmetics

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/blacksmith/EnterBlacksmithCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/blacksmith/EnterBlacksmithCmd.java
@@ -24,18 +24,34 @@ public class EnterBlacksmithCmd extends AbstractCmd
   @Override
   public void execute()
   {
-    if (character.getParty().getBlacksmith().isBusyCrafting())
-    {
-      CommandDelegate.execute(new SendMessageCmd(character, "The door to the Blacksmith's shop is locked; you can hear the sounds of clashing steel coming from his workshop. He must be busy with something."));
-      CommandDelegate.execute(new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE));
-    }
-
     Weapon collected = character.getParty().getBlacksmith().collectWeaponFor(character);
+
     if (null != collected)
     {
-      CommandDelegate.execute(new SendMessageCmd(character, "The Blacksmith smiles as you enter. \"Ah!\" he says, \"I've got something for you. I think you're gonna like it \""));
+      String msg;
+      if (character.getParty().getBlacksmith().isBusyCrafting())
+      {
+        msg = "The sounds of metal clashing stop when you rap the heavy iron door-knocker. After a brief pause, the Blacksmith opens the door. \"Aha,\" he says, wiping sweat from his forehead with a rag. \"It's ready. I'm quite proud of this piece. Now if you'll excuse me, I've got another commission in the furnace.\"";
+      }
+      else
+      {
+        msg = "The Blacksmith looks up and smiles as you enter. \"Ah!\" he says, \"I've got something for you. I think you're gonna like it \"";
+      }
+      CommandDelegate.execute(new SendMessageCmd(character, msg));
       CommandDelegate.execute(new SendAdminMessageCmd(character.getName() + " is collecting " + collected.getName() + " from the Blacksmith"));
       CommandDelegate.execute(new DoGiveItemCmd(character.getParty().getBlacksmith(), character, collected));
     }
+    else
+    {
+      if (character.getParty().getBlacksmith().isBusyCrafting())
+      {
+        CommandDelegate.execute(new SendMessageCmd(character, "The door to the Blacksmith's shop is locked; you can hear the sounds of clashing steel coming from his workshop. He must be busy with something."));
+      }
+    }
+  }
+
+  if (character.getParty().getBlacksmith().isBusyCrafting())
+  {
+    CommandDelegate.execute(new MoveToLocationCmd(character, ELocation.VILLAGE_SQUARE));
   }
 }
