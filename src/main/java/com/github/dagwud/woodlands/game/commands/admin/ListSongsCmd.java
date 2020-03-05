7337fc1c0e4acdb056f94e132da25df9d547e717
dagwud <johndevenish@gmail.com>
2020-03-05 20:56:46 +0200

    List songs

---
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/ListSongsCmd.java
@@ -0,0 +1,28 @@
+package com.github.dagwud.woodlands.game.commands.locations.village;
+
+import com.github.dagwud.woodlands.game.CommandDelegate;
+import com.github.dagwud.woodlands.game.GameStatesRegistry;
+import com.github.dagwud.woodlands.game.commands.core.AbstractCmd;
+import com.github.dagwud.woodlands.game.commands.core.SendMessageCmd;
+import com.github.dagwud.woodlands.game.domain.ELocation;
+import com.github.dagwud.woodlands.game.domain.location.tavern.JukeBox;
+
+import java.util.List;
+
+public class ListSongsCmd extends AbstractCmd
+{
+  public ListSongsCmd(int chatId)
+  {
+    this.chatId = chatId;
+  }
+
+  @Override
+  public void execute()
+  {
+    JukeBox jukeBox = GameStatesRegistry.instance().getJukeBox();
+    for (String lyric : jukeBox.getLyrics())
+    {
+      CommandDelegate.execute(new SendMessageCmd(chatId, lyric);
+    }
+  }
+}
