8fd09b27358cfd733d742cfd995542050bfe63f9
dagwud <johndevenish@gmail.com>
2020-03-06 09:49:39 +0200

    Update StartWeaponCraftCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/locations/blacksmith/StartWeaponCraftCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/locations/blacksmith/StartWeaponCraftCmd.java
@@ -29,6 +29,7 @@ public class StartWeaponCraftCmd extends AbstractCmd
     Blacksmith blacksmith = craftFor.getParty().getBlacksmith();
     Weapon crafted = createCraftedWeapon();
     blacksmith.setBusyCrafting(craftFor, crafted);
+    blacksmith.setCraftingExpectedEndTime(Settings.BLACKSMITH_CRAFTING_TIME_MS);
     CommandDelegate.execute(new RunLaterCmd(Settings.BLACKSMITH_CRAFTING_TIME_MS, new FinishCraftingCmd<Weapon>(crafted, craftFor, blacksmith)));
     CommandDelegate.execute(new SendAdminMessageCmd("Blacksmith is crafting " + firstWeapon.getName() + " and " + secondWeapon.getName() + " into a " + crafted.getName() + " for " + craftFor.getName())); 
   }
