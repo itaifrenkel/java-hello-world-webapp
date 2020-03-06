764eddfd514b0caa55d26464cf25416b15ddfd32
dagwud <johndevenish@gmail.com>
2020-03-06 16:29:27 +0200

    Update AutomaticEncounterRoundCmd.java

--- src/main/java/com/github/dagwud/woodlands/game/commands/battle/AutomaticEncounterRoundCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/battle/AutomaticEncounterRoundCmd.java
@@ -2,7 +2,7 @@ package com.github.dagwud.woodlands.game.commands.battle;
 
 import com.github.dagwud.woodlands.game.CommandDelegate;
 
-import com.github.dagwud.woodlands.game.commands.RunLaterCmd;
+import com.github.dagwud.woodlands.game.command.RunLaterCmd;
 import com.github.dagwud.woodlands.game.PlayerState;
 import com.github.dagwud.woodlands.game.domain.Encounter;
 
