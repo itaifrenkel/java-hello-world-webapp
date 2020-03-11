88b3482ef68ced6f198ac36242495e9b755434a8
Evan Knowles <evanjamesknowles@gmail.com>
2020-03-11 16:46:29 +0200

    Fix bug in sparring achievement

--- src/main/java/com/github/dagwud/woodlands/game/domain/events/SparringEventRecipient.java
+++ src/main/java/com/github/dagwud/woodlands/game/domain/events/SparringEventRecipient.java
@@ -3,6 +3,7 @@ package com.github.dagwud.woodlands.game.domain.events;
 import com.github.dagwud.woodlands.game.CommandDelegate;
 import com.github.dagwud.woodlands.game.commands.character.UnlockAchievementCmd;
 import com.github.dagwud.woodlands.game.domain.EAchievement;
+import com.github.dagwud.woodlands.game.domain.PlayerCharacter;
 
 public class SparringEventRecipient implements EventRecipient<SparringEvent>
 {
@@ -10,6 +11,6 @@ public class SparringEventRecipient implements EventRecipient<SparringEvent>
   public void trigger(SparringEvent event)
   {
     CommandDelegate.execute(new UnlockAchievementCmd(event.getPlayerCharacter(), EAchievement.FISTICUFFS));
-    CommandDelegate.execute(new UnlockAchievementCmd(event.getPlayerCharacter(), EAchievement.FISTICUFFS));
+    CommandDelegate.execute(new UnlockAchievementCmd((PlayerCharacter) event.getLoser(), EAchievement.LOST_SPARRING));
   }
 }
