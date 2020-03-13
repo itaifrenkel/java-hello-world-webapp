d43aaa95183ffecc503a0b04743d4da7b9be53bd
dagwud <johndevenish@gmail.com>
2020-03-13 16:43:23 +0200

    Undo

--- src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchCharacterCmd.java
+++ src/main/java/com/github/dagwud/woodlands/game/commands/admin/PatchCharacterCmd.java
@@ -26,11 +26,6 @@ public class PatchCharacterCmd extends AbstractCmd
       return;
     }
 
-    if (character.getPlayedBy().getChatId() == 586271876L)
-    {
-      character.setName("TINY PENIS");
-    }
-
     if (null != character.getStats())
     {
       fixNegativeStats(character.getStats().getStrength(), "strength");
